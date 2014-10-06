798f9aaceb670fd6bd68fa504ea383abd81b7ddd
edwinj85 <edwin_jones@hotmail.co.uk>
2014-10-06 01:24:25 +0100

    added config.h

---
+++ msvc/config.h
@@ -0,0 +1,31 @@
+/* config.h for MSVC builds.  This is just the config.h created by
+ * configure. */
+
+/* Name of package */
+#define PACKAGE "teapot-doom"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "edwin@edwinjones.me.uk"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Teapot Doom"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Teapot Doom 1.0.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "teapot-doom"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0.0"
+
+/* Change this when you create your awesome forked version */
+#define PROGRAM_PREFIX "teapot-"
+
+/* Version number of package */
+#define VERSION "1.0.0"
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+/* #undef WORDS_BIGENDIAN */
+
